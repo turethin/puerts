32ea00a8b6d003144d9870baa02eb9dec3e45fb3
pop-ci <pop-ci@happyelements.com>
2023-12-15 15:39:26 +0800

    ModuleResolver 日志加耗时打印

--- unity/native_src/Src/JSEngine_Eval.cpp
+++ unity/native_src/Src/JSEngine_Eval.cpp
@@ -5,6 +5,7 @@
 * This file is subject to the terms and conditions defined in file 'LICENSE', which is part of this source code package.
 */
 #include <algorithm>
+#include <‌iostream>
 #include <‌chrono>
 #include "JSEngine.h"
 #include "Log.h"
